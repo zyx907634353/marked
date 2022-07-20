f555cd67b447f1254280696b4421f3c28061842c
zOomxig <41902191+zyx907634353@users.noreply.github.com>
2022-07-20 13:45:41 +0800

    Update EDataStore.sol
    new contract

--- contracts/EDataStore.sol
+++ contracts/EDataStore.sol
@@ -64,11 +64,9 @@ contract EDataStore {
   //}
 
 
-  //�����Ʒ��������Ϣ���Ӵ洢
+  //添加水印图像哈希
   function addMarkedHashForProduct(uint _productId,string _markedHash,string passwordByBuyer) public returns(address,string,uint){
-      //������ˮӡ���ͼƬ�Ĺ�ϣֵ���Ӧ�Ĳ�Ʒ��Ž��д洢
       getProductIdWithMarkedHash[_markedHash] = _productId;
-      //��ǰ�˴�����ˮӡ��ϣ�Ͳ�Ʒ�����Ϊ�� ��������ҵ�ַ ���д洢
       buyerForProduct[_productId][_markedHash] = msg.sender;
       Product memory product = stores[productIdInStore[_productId]][_productId];
 
@@ -76,40 +74,40 @@ contract EDataStore {
 
       Buyer storage buyer = addressForBuyer[msg.sender];
       if(buyer.boughtProductId == 0)
-        buyer.credit =100;                                              //���ι�����Ʒ������ֵ��Ϊ100
-      buyer.boughtProductId++;                                          //������Ʒ����++
-      buyer.buyerAddress = msg.sender;                                  //�洢�˻���ַ
+        buyer.credit =100;                                              
+      buyer.boughtProductId++;                                          
+      buyer.buyerAddress = msg.sender;                                  ַ
       buyer.boughtIdToProductId[buyer.boughtProductId] = _productId;    //boughtId => productId
       buyer.boughtIdToPasswordFinal[buyer.boughtProductId] = strConcat(product.passwordBySeller,passwordByBuyer);
-      buyer.lastDateForProduct[_productId] = now + 3 seconds;             //����ʱ����Ϊ����ʱ���������
+      buyer.lastDateForProduct[_productId] = now + 3 seconds;             
 
       buyer.passwordFinalToProductId[strConcat(product.passwordBySeller,passwordByBuyer)] = _productId;
       pwdToBuyer[strConcat(product.passwordBySeller,passwordByBuyer)] = buyer;
       return (msg.sender,_markedHash,_productId);
   }
-  //�ٱ�����
+  //惩罚金
   function fine(string _markedHash) view public returns(address,uint){
     uint finedId = getProductIdWithMarkedHash[_markedHash];
     address finedAddress = buyerForProduct[finedId][_markedHash];
     //finedAddress.transfer(msg.value);
     return (finedAddress,finedId);
   }
-  //��ȡ��Ʒ��Ϣ
+  //获取产品
   function getProduct(uint _productId) view public returns (uint,string,string,string,uint) {
     Product memory product = stores[productIdInStore[_productId]][_productId];
     return (product.id, product.name, product.imageLink, product.description,product.price );
   }
-  //��ȡ��Ʒ�۸�
+  //获取产品价格
   function getPrice(uint _productId) view public returns(uint){
     Product memory product = stores[productIdInStore[_productId]][_productId];
     return product.price;
   }
-  //��ȡˮӡ�����ͼƬ����
+  //获取密码图像
   function getPasswordAndImage(uint _productId,string _password) public view returns(string,string) {
       Product memory product = stores[productIdInStore[_productId]][_productId];
       return(strConcat(product.passwordBySeller,_password),product.imageLink);
   }
-  //�����Ʒ��Ϣ
+  //添加产品
   function addProductToStore(string _name, string _imageLink,string _description, uint _price,string _passwordBySeller) public {
 
     productIndex += 1;
@@ -118,7 +116,7 @@ contract EDataStore {
     stores[msg.sender][productIndex] = product;
     productIdInStore[productIndex] = msg.sender;
   }
-  //�ַ���ƴ�Ӻ���
+  //字符串拼接函数
   function strConcat(string _a, string _b) pure internal returns(string){
     bytes memory _ba = bytes(_a);
     bytes memory _bb = bytes(_b);
@@ -134,37 +132,36 @@ contract EDataStore {
     return msg.sender.balance;
   }
 
-  //��ȡ��Լ���
+  //获取合约余额
   function getContractBalance() public view returns(uint){
     return this.balance;
   }
-  //��ȡ����˻�
+  //获取卖家地址
   function getSellerAddress(uint _productId) public view returns(address){
     return productIdInStore[_productId];
   }
-  //��ȡ�˻��ѹ���Ĳ�Ʒ������
+  //获取产品ID
   function getBoughtProductId(address _buyerAddress) public view returns(uint){
       Buyer storage buyer = addressForBuyer[_buyerAddress];
       return buyer.boughtProductId;
   }
-//��ȡ����ҳҪչʾ����Ϣ
+//获取买家所有已购产品
 function getAllBoughtProductForBuyer(address _buyerAddress,uint _boughtProductId) public view returns(uint,uint,string,uint,uint){
   Buyer storage buyer = addressForBuyer[_buyerAddress];
   uint _productId = buyer.boughtIdToProductId[_boughtProductId];
   Product memory product = stores[productIdInStore[_productId]][_productId];
 
-  //���ﷵ�ص���ԭ��Ӧ����js������������
   uint fundedPrice = product.price;
 
   return (_boughtProductId,fundedPrice,product.name,buyer.lastDateForProduct[_productId],buyer.productFlag[_boughtProductId]);
 }
-    //��ȡ����ֵ
+    //获取信誉值
     function getBuyerCredit(address currentAddress) public view returns(uint){
         Buyer memory buyer = addressForBuyer[currentAddress];
         if(buyer.credit == 100) return 100;
           else return 0;
     }
-    //���ָ���Ĳ�Ʒ�Ƿ�ɻ���
+    //检查回收产品
     function checkFundedProduct(address currentAccount,uint _boughtProductId) public view returns(uint){
          Buyer storage buyer = addressForBuyer[currentAccount];
          uint _productId = buyer.boughtIdToProductId[_boughtProductId];
@@ -175,33 +172,33 @@ function getAllBoughtProductForBuyer(address _buyerAddress,uint _boughtProductId
          else return 0;
    }
 
-   //�Ӻ�Լת��Ѻ��
+   //补偿
    function refundForBuyer(uint totalFund) public payable{
         msg.sender.transfer(totalFund);
    }
-   //��ΥԼ�˻�������ֵ��Ϊ��
+   //重设信誉值
    function setCreditToNone(address finedAddress) public payable{
       Buyer storage buyer = addressForBuyer[finedAddress];
       buyer.credit = 0;
    }
-   //��������
+   //奖励
    function rewardForBlower(uint _amountOfFine) public payable{
       msg.sender.transfer(_amountOfFine);
    }
-   //����Ѻ����յı�־
+   //设置产品的回收标志
    function setProductFlag(uint _boughtProductId,address currentAccount) public{
          Buyer storage buyer = addressForBuyer[currentAccount];
          buyer.productFlag[_boughtProductId] = 1;
          //uint _productId = buyer.boughtIdToProductId[_boughtProductId];
    }
-   //���productId�Ƿ��boughtId����
+   //获取产品ID
    function getProductIdWithBoughtId(address buyerAddress,string pwd) public view returns(uint){
          Buyer storage buyer = addressForBuyer[buyerAddress];
          //buyer.productFlag[_boughtProductId] = 1;
          uint productId = buyer.passwordFinalToProductId[pwd];
          return productId;
    }
-   //���������ȡ��ҵ�ַ�Ͳ�ƷId
+   //获取买家密码
    function getBuyerAddressByPassword(string _password) public view returns(address,uint,uint){
        Buyer storage buyer = pwdToBuyer[_password];
        uint productId = buyer.passwordFinalToProductId[_password];
